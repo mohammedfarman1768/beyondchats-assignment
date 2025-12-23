e95e61add9aea4a5bc7769bd696e8af85ed43e5f
Mohammed Farman <farmankhan1768@gmail.com>
2025-12-24 04:06:45 +0530

    deployment error two

--- laravel-api/scripts/deploy.sh
+++ laravel-api/scripts/deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+# Generate the autoloader and run discovery now that we are live
+composer dump-autoload --optimize
+
 # Run database migrations
 php artisan migrate --force
 
-# Cache configurations for speed
+# Clear and set caches
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
