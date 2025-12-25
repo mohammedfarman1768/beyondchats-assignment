d44469d4a2f949259a71c912647a643715a15e91
Mohammed Farman <farmankhan1768@gmail.com>
2025-12-25 18:01:16 +0530

    database

--- laravel-api/config/database.php
+++ laravel-api/config/database.php
@@ -83,20 +83,20 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+'pgsql' => [
+    'driver' => 'pgsql',
+    'url' => env('DATABASE_URL'), // This reads the URL you set in Render
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'require', // Add this line for NeonDB
+],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
